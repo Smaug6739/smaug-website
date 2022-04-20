8efa5d47668ad9508586859bfcea1170a7828ffb
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-20 15:36:53 +0200

    Improve CI

---
+++ prod.ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "smaug-wbs",
+      cwd: "./output/server",
+      script: "node index.mjs",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
