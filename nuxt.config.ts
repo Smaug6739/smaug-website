80237e4d65895fd98e9b535a9f394cf2c3cba34b
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-20 13:53:33 +0200

    Improve SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,15 +15,11 @@ export default defineNuxtConfig({
           content:
             "Hi, I'm Smaug#6739, full-stack developer from France, I develop also software and diffrent libraries.",
         },
+        // Open Graph / Facebook
         {
           hid: "og:title",
           name: "og:title",
           content: "Smaug#6739",
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: `/seo/img.png`,
         },
         {
           hid: "og:description",
@@ -37,9 +33,16 @@ export default defineNuxtConfig({
           content: `https://smaug-6739.com`,
         },
         {
-          hid: "twitter:card",
-          property: "twitter:card",
+          hid: "og:image",
+          property: "og:image",
           content: `/seo/img.png`,
+        },
+
+        // Twitter
+        {
+          hid: "twitter:title",
+          property: "twitter:title",
+          content: `Smaug#6739`,
         },
         {
           hid: "twitter:creator",
@@ -47,9 +50,24 @@ export default defineNuxtConfig({
           content: `Smaug#6739`,
         },
         {
+          hid: "og:url",
+          property: "og:url",
+          content: `https://smaug-6739.com`,
+        },
+        {
           hid: "twitter:description",
           property: "twitter:description",
           content: `Hi, I'm Smaug#6739, full-stack developer from France, I develop also software and diffrent libraries.`,
+        },
+        {
+          hid: "twitter:card",
+          property: "twitter:card",
+          content: `/seo/img.png`,
+        },
+        {
+          hid: "twitter:image",
+          property: "twitter:image",
+          content: `/seo/img.png`,
         },
       ],
     },
