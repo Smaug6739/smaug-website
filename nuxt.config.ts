116276c9c875258479ba93bb9e81b38982e97f28
Raphael Veauville <veauville.raphael@gmail.com>
2022-10-25 20:02:26 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,12 +16,12 @@ export default defineNuxtConfig({
 
         { name: "robots", content: "follow, index" },
         { name: "theme-color", content: "#00BFFF" },
-        { name: "description", content: "Hi, I'm Smaug6739, full-stack developer from France, I develop also software and diffrent libraries." },
+        { name: "description", content: "Hi, I'm Smaug6739, full-stack developer from France, I also develop software and different libraries." },
 
         // Open Graph / Facebook
         { property: "og:type", content: "website" },
         { property: "og:title", content: "Smaug6739" },
-        { property: "og:description", content: "Hi, I'm Smaug6739, full-stack developer from France, I develop also software and diffrent libraries." },
+        { property: "og:description", content: "Hi, I'm Smaug6739, full-stack developer from France, I also develop software and different libraries." },
         { property: "og:url", content: `https://smaug-6739.dev` },
         { property: "og:keywords", content: "Developer, Full-Stack, Software, Smaug6739" },
         { property: "og:image", content: `https://smaug-6739.dev/seo/smaug.png` },
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
         { name: "twitter:creator", content: `@Smaug6739` },
         { name: "twitter:site", content: `@Smaug6739` },
         { name: "twitter:url", content: `https://smaug-6739.dev` },
-        { name: "twitter:description", content: `Hi, I'm Smaug6739, full-stack developer from France, I develop also software and diffrent libraries.` },
+        { name: "twitter:description", content: `Hi, I'm Smaug6739, full-stack developer from France, I also develop software and different libraries.` },
         //{ name: "twitter:card",  content: `summary_large_image` },
         { name: "twitter:image", content: `https://smaug-6739.dev/seo/smaug.png` },
       ],
